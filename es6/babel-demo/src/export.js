0f4fbd85431a3b2d873b639fe56879a78a82acdb
unknown <you@example.com>
2020-08-11 17:05:38 +0800

    npm

---
+++ es6/babel-demo/src/export.js
@@ -0,0 +1,7 @@
+// export let a = "hello";
+// export class Person{
+//   constructor(name){
+//     this.name = name
+//   }
+//   Sleep(){
+//     console.log(this.name +
---
+++ es6/babel-demo/src/import.js
@@ -0,0 +1,4 @@
+// import {a as b,Person} from "./export"
+// console.log(b);
+// let p1 = new Person("小a");
+//
